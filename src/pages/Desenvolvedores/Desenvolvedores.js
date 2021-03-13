c6f44d160ca13186c7ef5c419e4bd53c2dfad92b
Marcos <70656788+Marcos03324@users.noreply.github.com>
2021-03-13 17:58:00 -0300

    Update Desenvolvedores.js

--- src/pages/Desenvolvedores/Desenvolvedores.js
+++ src/pages/Desenvolvedores/Desenvolvedores.js
@@ -15,8 +15,8 @@ function Desenvolvedores(){
                     <img src={perfil} alt="Marcos" />
                     </div>
                     <p>
-                        Meu Nome é Marcos Henrique, sou Aluno do curso Tecnico em Informatica onde , tenho conhecimento basico em PHP, HTML, CSS, JAVA e REACT.<br/> Ja me formei em cursos de 
-                        AutoCad e Operador de computador. Sou dono de duas "empresas" uma chamada de MH Dev, relacionada a criação
+                        Meu Nome é Marcos Henrique, sou Aluno do curso Tecnico em Informatica, onde tenho conhecimento basico em PHP, HTML, CSS, JAVA e REACT.<br/> Ja me formei em cursos de 
+                        AutoCad e Operador de computador. Sou o proprietário de duas "empresas" uma chamada de MH Dev, relacionada a criação
                         de sites e edição de imagens,e a outra chamada MH computer, onde realizo
                         manutenção e formatação de computadores.
                     </p>
